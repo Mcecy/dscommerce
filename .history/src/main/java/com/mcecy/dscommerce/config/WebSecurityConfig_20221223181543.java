b0af993f87a9983ac39584ff1525737a290c8e80
mcecy <ceciliacadengue@gmail.com>
2022-12-23 18:25:08 -0300

    Configurando OAuth2 e JWT

--- .history/src/main/java/com/mcecy/dscommerce/config/WebSecurityConfig_20221223181543.java
+++ .history/src/main/java/com/mcecy/dscommerce/config/WebSecurityConfig_20221223181543.java
@@ -1,14 +1,10 @@
 package com.mcecy.dscommerce.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
